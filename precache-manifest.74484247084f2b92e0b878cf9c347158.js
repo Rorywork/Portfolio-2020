7d7136aa09b087d6329017bc165d66d8dba367cf
Rorywork <rorywork95@outlook.com>
2020-03-02 13:40:05 +0000

    Updates

---
+++ precache-manifest.74484247084f2b92e0b878cf9c347158.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4ec2d900762a236ea33316da8206489",
+    "url": "/portfolio-2020/index.html"
+  },
+  {
+    "revision": "64415691b7e578e92f72",
+    "url": "/portfolio-2020/static/css/main.b5905523.chunk.css"
+  },
+  {
+    "revision": "61d36652478ab495f5b7",
+    "url": "/portfolio-2020/static/js/2.3cf12bac.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/portfolio-2020/static/js/2.3cf12bac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64415691b7e578e92f72",
+    "url": "/portfolio-2020/static/js/main.5553741d.chunk.js"
+  },
+  {
+    "revision": "2e8056e34624077d15e7",
+    "url": "/portfolio-2020/static/js/runtime-main.bb0a96ad.js"
+  },
+  {
+    "revision": "bc508a4d59ce0ff943ab5c41a1a80917",
+    "url": "/portfolio-2020/static/media/kiss-and-makeup.bc508a4d.PNG"
+  },
+  {
+    "revision": "0a72d68f31c68bae1f090f47a550249f",
+    "url": "/portfolio-2020/static/media/pokemon-master.0a72d68f.PNG"
+  },
+  {
+    "revision": "0e6fd312050b904a3413121595aecbbe",
+    "url": "/portfolio-2020/static/media/quotepad.0e6fd312.PNG"
+  },
+  {
+    "revision": "7e2976521321a41618678233452ff088",
+    "url": "/portfolio-2020/static/media/roryCV.7e297652.pdf"
+  },
+  {
+    "revision": "1105c2c1a894fd88359150a42179ac24",
+    "url": "/portfolio-2020/static/media/the-shadows.1105c2c1.PNG"
+  },
+  {
+    "revision": "5910ae89e7a4abbc7977df08d7816eda",
+    "url": "/portfolio-2020/static/media/warcraft.5910ae89.PNG"
+  }
+]);
