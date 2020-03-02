1d42172009c6e3030bf4f87dc8ba13b7bdceec65
Rorywork <rorywork95@outlook.com>
2020-03-02 14:14:58 +0000

    Added rel to link

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,7 +5,7 @@ const Button = ({ type, href }) => {
 
     return (
         <div>
-            <a className="button" href={href} target="_blank">
+            <a className="button" href={href} target="_blank" rel="noopener noreferrer" >
                 <i class={`fab fa-${type}`}></i>
             </a>
         </div >
