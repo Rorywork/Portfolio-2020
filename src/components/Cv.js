2f7552bcb8867088ae425ad0c894c839441963f9
Rorywork <rorywork95@outlook.com>
2020-03-02 13:48:47 +0000

    Added rekl to links

--- src/components/Cv.js
+++ src/components/Cv.js
@@ -6,7 +6,7 @@ import RoryCV from "../documents/roryCV.pdf";
 const Cv = () => {
     return (
         <div>
-            <a className="cv" href={RoryCV} target="_blank">CV</a>
+            <a className="cv" href={RoryCV} target="_blank" rel="noopener noreferrer" >CV</a>
         </div>
     )
 }
