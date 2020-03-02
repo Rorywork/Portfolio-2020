e75e92e3d067c1f23685c2a64d84477ab8a962b7
Rorywork <rorywork95@outlook.com>
2020-03-02 15:57:04 +0000

    Made btn on overlay link to code and divs link to the sites

--- src/components/PortfolioItem.js
+++ src/components/PortfolioItem.js
@@ -3,17 +3,17 @@ import React from "react";
 import TechList from "./TechList"
 
 
-const PortfolioItem = ({ title, imgSrc, imgAlt, desc, code, tech }) => {
+const PortfolioItem = ({ title, imgSrc, imgAlt, desc, code, tech, url }) => {
 
     return (
-        <div className="portfolio-item">
+        <div className="portfolio-item" onClick={() => { window.open(url) }}>
             <h1 className="item-title">{title}</h1>
             <img className="ui image img" src={imgSrc} alt={imgAlt}></img>
             <div className="overlay">
                 <TechList code={code} tech={tech} />
             </div>
             <p className="item-desc">{desc}</p>
-        </div>
+        </div >
     );
 }
 
--- src/components/PortfolioList.js
+++ src/components/PortfolioList.js
@@ -17,49 +17,55 @@ const PortfolioList = () => {
                 title="Kiss and Makeup"
                 imgSrc={kissAndMakeup}
                 imgAlt="Asian bridal website"
-                desc="A stylish marketing website"
+                desc="Asian bridal hair and beauty static site build for a client."
                 code="https://github.com/Rorywork/kiss-and-makeup"
                 tech={["HTML", "CSS", "Bootstrap", "Jquery"]}
+                url="https://rorywork.github.io/kiss-and-makeup/"
             />
             <PortfolioItem
                 title="Pokemon Master"
                 imgSrc={pokemonMaster}
                 imgAlt="Api based Pokemon game"
-                desc="This is a different project"
+                desc="Pokemon themed guessing game."
                 code="https://github.com/Rorywork/pokemon-master"
                 tech={["HTML", "CSS", "Bootstrap Grid", "Javascript", "PokeAPI"]}
+                url="https://rorywork.github.io/pokemon-master/"
             />
             <PortfolioItem
                 title="The Shadows"
                 imgSrc={theShadows}
                 imgAlt="Flask Photography website"
-                desc="Flask Photography website"
+                desc="Website for users to share black and white photos."
                 code="https://github.com/Rorywork/The-Shadows"
                 tech={["HTML", "CSS", "Javascript", "Python", "Flask", "MongoDB"]}
+                url="https://the-shadows.herokuapp.com/"
             />
             <PortfolioItem
                 title="Quotepad"
                 imgSrc={quotepad}
                 imgAlt="Django Full stack website"
-                desc="Django Full stack website"
+                desc="Generate PDF quotes for customers of boiler surveys."
                 code="https://github.com/Rorywork/bquotepad"
                 tech={["HTML", "CSS", "Javascript", "Python", "Django", "MySql"]}
+                url="https://rorytesting.pythonanywhere.com/"
             />
             <PortfolioItem
                 title="Warcraft"
                 imgSrc={warcraft}
                 imgAlt="Wow Interface"
-                desc="Built using SASS and JS"
+                desc="Building a trading interface based upon a popular MMORPG."
                 code="https://rorywork.github.io/wow-interface/"
                 tech={["HTML", "CSS", "Javascript", "SASS"]}
+                url="https://rorywork.github.io/wow-interface/"
             />
             <PortfolioItem
                 title="Sports Club Video Player"
                 imgSrc={bgsuns}
                 imgAlt="BG Suns Video Player"
-                desc="Built using SASS and JS"
+                desc="Video player utilising Youtube API for a sports team."
                 code="https://rorywork.github.io/wow-interface/"
                 tech={["React", "HTML", "CSS", "Youtube API"]}
+                url="https://rorywork.github.io/BGSuns-react-app/"
             />
         </div>
     );
